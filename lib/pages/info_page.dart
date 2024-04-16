7d9505fe131b46e7b66eea8319dc5f3740e1105a
Johan Ramos <johan.ramos.m@tecsup.edu.pe>
2024-04-16 12:03:19 -0500

    [update]structure and design

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -1,80 +1,127 @@
 import 'package:descub_espaciounno/util/colors.dart';
-import 'package:descub_espaciounno/widgets/info/info_profile.dart';
-import 'package:descub_espaciounno/widgets/info/info_text.dart';
-import 'package:descub_espaciounno/widgets/info/info_title.dart';
-import 'package:descub_espaciounno/widgets/menu/location_geo_button.dart';
-import 'package:descub_espaciounno/widgets/menu/social_media_button.dart';
+import 'package:descub_espaciounno/widgets/info/acerca_de_section.dart';
+import 'package:descub_espaciounno/widgets/info/explora_section.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class InfoPage extends StatelessWidget {
+class InfoPage extends StatefulWidget {
   const InfoPage({super.key});
 
   @override
+  _InfoPageState createState() => _InfoPageState();
+}
+
+class _InfoPageState extends State<InfoPage> {
+  int _currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return AnnotatedRegion(
+      value: const SystemUiOverlayStyle(
+          systemNavigationBarColor: AppColors.appLightS,
+          statusBarColor: AppColors.primaryUNNO,
+          statusBarBrightness: Brightness.light,
+          statusBarIconBrightness: Brightness.light
+      ),
+      child: Scaffold(
+        backgroundColor: AppColors.appLightS,
         appBar: AppBar(
-          backgroundColor: AppColors.appLight,
-          elevation: 0,
-          foregroundColor: AppColors.appDark,
-        ),
-        body: Container(
-            height: MediaQuery.of(context).size.height,
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    const InfoTitle(),
-                    const InfoProfile(),
-                    const InfoText(),
-                    Container(
-                      margin: const EdgeInsets.symmetric(vertical: 20.0),
-                      height: 160,
-                      alignment: Alignment.center,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Image.asset('assets/images/logo_descub.png',
-                          width: 120),
-                          Icon(Icons.favorite, color: Colors.pink, size: 100),
-                          Image.asset('assets/images/espacio_unno_logo.png',
-                              width: 120)
-                        ],
-                      ),
-                    ),
-                    const Spacer(),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.85,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.only(bottom: 40.0),
-                      decoration: BoxDecoration(
+          backgroundColor: AppColors.primaryUNNO,
+          elevation: 3,
+          toolbarHeight: 100.00,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              RichText(
+                text: const TextSpan(
+                  text: 'espacio ',
+                  style: TextStyle(
+                      color: AppColors.appLight,
+                      fontSize: 14.0,
+                      fontFamily: 'Noto Sans'
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'UNNO',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
                         color: AppColors.appLight,
-                        borderRadius: BorderRadius.circular(8),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.2),
-                            blurRadius: 4,
-                            offset: const Offset(0, 2),
-                          ),
-                        ],
-                      ),
-                      child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          SocialMediaButton(username: "espaciounno", socialMedia: "instagram", urlIcon: "instagram_icon", color: AppColors.appDark,),
-                          LocationGeoButton(latitude: -12.1493747, longitude: -77.0228592, urlIcon: "marker_icon", color: AppColors.appDark,),
-                          SocialMediaButton(username: "espaciounno", socialMedia: "tiktok", urlIcon: "tiktok_icon", color: AppColors.appDark,),
-                        ],
+                        fontSize: 16.0,
                       ),
                     ),
                   ],
                 ),
               ),
-            )
-        )
+              Image.asset('assets/images/espacio_unno_logo_light.png',
+                  width: 100)
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: _currentIndex == 0 ? AppColors.primaryUNNO : Colors.transparent,
+                        width: 2.0,
+                      ),
+                    ),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _currentIndex = 0;
+                      });
+                    },
+                    style: TextButton.styleFrom(
+                      primary: _currentIndex == 0 ? Colors.black : Colors.grey,
+                      textStyle: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    child: const Text('Explora', style: TextStyle(fontSize: 18.0)),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: _currentIndex == 1 ? AppColors.primaryUNNO : Colors.transparent,
+                        width: 2.0,
+                      ),
+                    ),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _currentIndex = 1;
+                      });
+                    },
+                    style: TextButton.styleFrom(
+                      primary: _currentIndex == 1 ? Colors.black : Colors.grey,
+                      textStyle: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    child: const Text('Acerca de', style: TextStyle(fontSize: 18.0)),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 15.0),
+            Expanded(
+              child: IndexedStack(
+                index: _currentIndex,
+                children: const [
+                  ExploraSection(),
+                  AcercaDeSection(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
